8d8f591701ba5ef5a02ac7d9ed6fb407456c165f
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-27 15:50:14 +1000

    Update the seed file that was generated.

---
+++ seeders/20190626101342-e2e-data.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { Author, Book } = require('../sequelize');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Author.create({
+      first_name: "Annabel",
+      last_name: "Crabb"
+    }).then((author) => {
+      return Book.create({
+        title: "The Wife Drought",
+        author_id: author.id
+      });
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    Book.findOne({
+      where: {
+        title: 'The Wife Drought'
+      }
+    }).then((book) => {
+      if (book) {
+        return Book.destroy({
+          where: {
+            id: book.id
+          }
+        });
+      }
+      return true;
+    });
+
+    return Author.findOne({
+      where: {
+        first_name: 'Annabel',
+        last_name: 'Crabb'
+      }
+    }).then((author) => {
+      if (author) {
+        return Author.destroy({
+          where: {
+            id: author.id
+          }
+        });
+      }
+      return true;
+    });
+  }
+}
