97de3b780ebaa85d78103edcea21c4def9a48d64
Monica Mendes <monicamendesmontanha@gmail.com>
2019-06-27 15:48:06 +1000

    Update the test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,7 +1,12 @@
 describe('End to End Tests', function() {
   it('Logs in', function() {
-    cy.visit('/login', { timeout: 5000 });
-    cy.get('button').click({ position: 'topLeft' });
+    cy.login('foo', 'bar');
     cy.get('li.book').should('have.length', 3);
-  })
-})
+  });
+
+  it('unable to login with incorrect credentials', function() {
+    cy.login('xxx', 'yyy');
+    cy.get('li.book').should('have.length', 0);
+  });
+});
+
